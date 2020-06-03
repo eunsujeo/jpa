6dc536851c46f8ad9590e0fec29ef6ba3b16bd53
silverspoon8 <silverspoon@vlending.co.kr>
2020-06-04 07:37:00 +0900

    first commit

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,41 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        //EntityManagerFactory db 당 하나만 생성해서 어플리케이션 전체에서 공유
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+
+        //EntityManager 요청이 올 때마다 사용하고 버린다. 쓰레드간에 공유 X( 사용하고 버려야 한다.)
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        //JPA 모든 데이터 변경은 트랜잭션 안에서 실행해야 한다.
+        //우리가 트랜잭션을 안걸어도 DB 내부적으로 트랜잭션을 가지고 있다.
+
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        try {
+//            Member member = entityManager.find(Member.class, 1L);
+
+//            member.setName("hihihihi");
+            List<Member> result = entityManager.createQuery("select m from Member m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(1)
+                    .getResultList();
+
+            result.forEach((r)-> System.out.println(r.getId()));
+
+            // commit 하기 직전에 체크해서 데이터 변경된 걸 보고 업데이트 쿼리를 날린다.
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        } finally {
+            entityManager.close();
+        }
+
+        entityManagerFactory.close();
+    }
+}
